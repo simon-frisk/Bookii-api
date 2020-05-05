2215da535fc01a06c448db880e918dd7a9bc3c86
Simon Frisk <simon@simonfrisk.com>
2020-05-05 11:50:37 +0200

    fix

--- src/user/user.resolver.js
+++ src/user/user.resolver.js
@@ -2,13 +2,12 @@ const { isBookIdTypeISBN, doesISBNBookIdsMatch } = require('../book/bookIdUtil')
 
 module.exports = {
   async feedBooks(user, { bookId, _id }) {
-    if (_id) return user.feedBooks._id(_id)
-    else
-      return (isBookIdTypeISBN(bookId)
-        ? user.feedBooks.filter(feedBook =>
-            doesISBNBookIdsMatch(feedBook.bookId, bookId)
-          )
-        : user.feedBooks
-      ).sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+    let feedBooks = user.feedBooks
+    if (_id) feedBooks = user.feedBooks.id(_id) ? [user.feedBooks.id(_id)] : []
+    if (isBookIdTypeISBN(bookId))
+      feedBooks = feedBooks
+        .filter(feedBook => doesISBNBookIdsMatch(feedBook.bookId, bookId))
+        .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+    return feedBooks
   },
 }
