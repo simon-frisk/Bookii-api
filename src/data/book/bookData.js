43f53d333ba9e54994b5fd638f9a932bfe9e7a0e
Simon Frisk <simon@simonfrisk.com>
2020-07-13 14:57:27 +0200

    Remove openlibrary

--- src/data/book/bookData.js
+++ src/data/book/bookData.js
@@ -1,5 +1,4 @@
 const googleBooks = require('./googleBooks')
-const openLibrary = require('./openLibrary')
 const nytBooks = require('./nytBooks')
 const wikipedia = require('./wikipedia')
 const googleBooksDB = require('./googleBooksDB')
@@ -7,8 +6,7 @@ const { ApolloError } = require('apollo-server')
 
 module.exports = {
   getByBookId: async bookId => {
-    let data = await openLibrary.getBookDataFromBookId(bookId)
-    if (!data) data = await googleBooksDB.getBookData(bookId)
+    let data = await googleBooksDB.getBookData(bookId)
     if (!data) {
       data = await googleBooks.getBookDataFromBookId(bookId)
       if (!data) throw new ApolloError('Failed to get book data')
