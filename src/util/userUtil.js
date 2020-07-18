60134c0da558f51d73cba36e94eaeb805582ab3f
Simon Frisk <simon@simonfrisk.com>
2020-07-18 16:46:10 +0200

    Change name regex

--- src/util/userUtil.js
+++ src/util/userUtil.js
@@ -14,20 +14,10 @@ module.exports = {
     return fixedEmail
   },
   validateAndFixName(name) {
-    name = name.trim()
-    const names = name.split(' ')
-    if (!(names.length === 2))
-      throw new UserInputError('Name has to consist of first- and lastname')
-    const firstNameLetters = names[0].toLowerCase().split('')
-    firstNameLetters[0] = firstNameLetters[0].toUpperCase()
-    const lastNameLetters = names[1].toLowerCase().split('')
-    lastNameLetters[0] = lastNameLetters[0].toUpperCase()
-    const fixedName = firstNameLetters
-      .join('')
-      .concat(' ', lastNameLetters.join(''))
-    if (!nameRegex.test(fixedName))
-      throw new UserInputError('Name has to consist of first- and lastname')
-    return fixedName
+    if (!name.includes(' ')) throw new UserInputError('Full name required')
+    if (/[\d"#¤%&/()=?^*~€$£@}{,.]/.test(name))
+      throw new UserInputError('Name contains forbidded characters')
+    return name.trim()
   },
   async validatePasswordAndCreateHash(password) {
     if (password.length < 6)
@@ -39,5 +29,4 @@ module.exports = {
   },
 }
 
-const nameRegex = /^[A-ZÅÄÖ][a-zaåä]+ [A-ZÅÄÖ][a-zåäö]+$/ //TODO: Make this international etc.
 const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
