61309d8f90875487acaefe5daedef5896e2402be
Simon Frisk <simon@simonfrisk.com>
2020-07-19 11:01:10 +0200

    Add storage youtube api

--- src/data/book/youtubeData.js
+++ src/data/book/youtubeData.js
@@ -1,15 +1,44 @@
 const axios = require('axios')
+const { Datastore } = require('@google-cloud/datastore')
+
+const datastore = new Datastore()
 
 exports.getVideoIds = async title => {
   try {
-    const { data } = await axios.get(createUrl(title))
-    return data.items.map(video => video.id.videoId)
+    const dbIds = await getVideoIdsFromDB(title)
+    if (dbIds) return dbIds
+    const apiIds = await getVideoIdsFromAPI(title)
+    await storeVideoIds(title, apiIds)
+    return apiIds
   } catch (error) {
     console.log(error.message)
     return []
   }
 }
 
+async function getVideoIdsFromAPI(title) {
+  const { data } = await axios.get(createUrl(title))
+  return data.items.map(video => video.id.videoId)
+}
+
+async function getVideoIdsFromDB(title) {
+  const [videoIds] = await datastore.get(
+    datastore.key(['YoutubeVideos', title])
+  )
+  return videoIds.ids
+}
+
+async function storeVideoIds(title, ids) {
+  const entity = {
+    key: datastore.key(['YoutubeVideos', title]),
+    data: {
+      ids,
+    },
+    excludeFromIndexes: ['ids'],
+  }
+  await datastore.upsert(entity)
+}
+
 function createUrl(title) {
-  return `https://www.googleapis.com/youtube/v3/search?part=id&maxResults=5&q=${title}+book&type=video&key=${process.env.GOOGLE_API_KEY}`
+  return `https://www.googleapis.com/youtube/v3/search?part=id&q=${title}+book&type=video&key=${process.env.GOOGLE_API_KEY}`
 }
